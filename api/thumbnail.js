e222c5255923a551251352c9fdcc7be773f906cf
Nicolas Guillen <guillen_nicolas@live.com>
2022-08-20 03:13:53 -0600

    🚧 last attempt for today

--- api/thumbnail.js
+++ api/thumbnail.js
@@ -24,7 +24,11 @@ export default async function handler(req, res) {
 
   const pageTemplate = getRenderedPage(params);
   const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
   try {
